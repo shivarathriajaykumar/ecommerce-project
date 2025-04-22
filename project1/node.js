c63fe4ca22a1e7407196598511f2f67d06f726af
ajay <sajay82475@email.com>
2025-04-22 16:01:44 +0530

    main

---
+++ project1/node.js
@@ -0,0 +1,18 @@
+// let productsdata=fetch('https://www.dummyjson.com/products')
+// async function Data(){
+//     let productsapi=await productsdata
+//     console.log(productsapi);
+
+//     let {products}=await productsapi.json()
+//     console.log(products)
+//     products.map((item)=>{
+//         document.body.innerHTML+=`<div id='products'>
+//         <h3> ${item.title} <h3>
+//         <img src=${item.thumbnail}
+//         <p>${item.description}<p></div>`
+//     })
+    
+// }
+// Data()
+// console.log(productsdata)
+
